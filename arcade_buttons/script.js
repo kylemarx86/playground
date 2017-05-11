0043cf809f18d1a9f4a6f6ef3f8188699fe72e29
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-05-10 19:06:43 -0700

    add next and prev page functionality with animations.

--- arcade_buttons/script.js
+++ arcade_buttons/script.js
@@ -1,17 +1,48 @@
+var page_arr = ['.first', '.second', '.third'];
+var curr_page = null;
+var new_page = null;
+
 $(document).ready(function(){
-    // console.log('ready');
+    curr_page = 0;
     apply_button_handlers();
 });
 
 function apply_button_handlers(){
-    $('.arcade_btn.red').click(previous_color);
-    $('.arcade_btn.blue').click(next_color);
+    $('.arcade_btn.red').click(get_prev);
+    $('.arcade_btn.blue').click(get_next);
 }
 
-function previous_color(){
-    console.log('prev');
-};
-function next_color(){
-    console.log('next');
-};
+function button_disable_and_reenable(){
+  $('#prev').off();
+  $('#next').off();
+  setTimeout(function(){
+    $('#prev').click(get_prev);
+    $('#next').click(get_next);
+  }, 1000);
+}
+function get_prev(){
+  button_disable_and_reenable();
+  new_page = (curr_page + page_arr.length - 1) % page_arr.length;
+  update_page(new_page);
+}
+function get_next(){
+  button_disable_and_reenable();
+  new_page = (curr_page + 1) % page_arr.length;
+  update_page(new_page);
+}
+function update_page(new_page){
+  $(page_arr[curr_page]).toggleClass('shrunk grown');
+  $(page_arr[new_page]).toggleClass('shrunk grown');
+  toggle_hidden(curr_page, new_page);
+  curr_page = new_page;
+  new_page = null;
+}
 
+function toggle_hidden(curr_page, new_page){
+    setTimeout(function(){
+        // console.log('curr_page:', curr_page);
+        // console.log('new_page:', new_page);
+        $(page_arr[curr_page]).toggleClass('hidden');
+        $(page_arr[new_page]).toggleClass('hidden');
+    }, 480);
+}
