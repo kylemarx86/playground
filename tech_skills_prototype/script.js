631ea40abaaad27eb49373f5608bbd5788e82ddf
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-05-17 15:21:09 -0700

    basic spinnning functionality.

--- tech_skills_prototype/script.js
+++ tech_skills_prototype/script.js
@@ -3,20 +3,28 @@ $(document).ready(function(){
 });
 
 function applyClickHandlers(){
-    // console.log('click ready');
     $('.tech').click(toggleSelected($(this)));
 }
 
 function toggleSelected(tech){
-    // $(this.)
-    // console.log('clicked');
-    // console.log('this: ',tech);
     $('.tech').click(function(){
         console.log('clicked');
-        // console.log('count tech elements', $('.tech').length);
         var $elt_index = $(this).parent().find(this).index();
-        console.log('elt num: ', $elt_index);
+
+        console.log('elt index: ', $elt_index);
         $('.tech').removeClass('selected');
+
+        var elt_count = $('.circle-container li.tech').length;
+
+        for(var i = 0; i <= elt_count; i++){
+            var end_rot = 360 / elt_count * (i - $elt_index) + 360;
+            console.log('i: ', i, 'rot: ', end_rot);
+            $(`.circle-container > *:nth-of-type(${i+1})`).css({
+                'transform': `rotate(${end_rot}deg)   translate(12.5em)  rotate(-${end_rot}deg)`,
+                // 'transition-duration': '0.5s'
+            });
+        }
+        
         $(this).toggleClass('selected');
     });
 }
