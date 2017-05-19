fd98fb0b9b5f6056c6aaa74af6e0199a46785c49
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-05-19 11:02:46 -0700

    tech skills - eliminated end points from rotation.

--- tech_skills_prototype/script.js
+++ tech_skills_prototype/script.js
@@ -1,9 +1,23 @@
-var curr_rot = null;
+// var curr_rot = null;
+// var last_rot = null;
+var rot_arr;    //keeps track of the rotations of each elt as they spin around in circular path
 
 $(document).ready(function(){
-    curr_rot = 0;
+    // curr_rot = 0;
+    // last_rot = 0;
+    rot_arr = [];
+    initializeLocations();
     applyClickHandlers();
 });
+
+function initializeLocations(){
+    var item_count = $('.circle-container .tech').length;
+    // console.log(item_count);
+    for(var i = 0; i < item_count; i++){
+        $(`.circle-container .tech:nth-of-type(${i+1})`).attr('loc', i);
+        rot_arr[i] = 360 / item_count * i;
+    }
+}
 
 function applyClickHandlers(){
     $('.tech').click(toggleSelected($(this)));
@@ -14,73 +28,69 @@ function toggleSelected(tech){
         
         console.log('clicked');
         
-        var $elt_index = $(this).parent().find(this).index();
-        console.log('elt index: ', $elt_index);
+        
+        //determine new rotation based on the location index not the element index
+        // var $elt_index = $(this).parent().find(this).index();
+        // console.log('elt index: ', $elt_index);
+        var loc_index = $(this).attr('loc');
+        console.log('loc index: ', loc_index);
+
+        var elt_count = $('.circle-container li.tech').length;
+
+        // console.log($('.circular-container '));
+
+        // determine which way to spin (cw or ccw) and by how much by first determining which is the closer path.
+            // this can be done by seeing if the clicked element is under or over the halfway mark of the number of elements
+
+
+        // var new_rot = $elt_index <= elt_count / 2 ? -1 * $elt_index * 360 / elt_count : (elt_count - $elt_index) * 360 / elt_count;
+        var new_rot = loc_index <= elt_count / 2 ? -1 * loc_index * 360 / elt_count : (elt_count - loc_index) * 360 / elt_count;
+        console.log(`new rot: ${new_rot}`);
 
         // whether or not to spin cw or ccw is determined by the shortest path to the end. 
             // this can be determined by a portion of the transformation matrix in the css.
             // if this value is greater than or equal to zero the clicked element is located on the lower half of the circle and should be rotated ccw,
             // else the clicked element and all others should be rotated cw.
-        var clicked_matrix_part = Math.round(parseFloat($(this).css('transform').split(', ')[5]));
+        // var clicked_matrix_part = Math.round(parseFloat($(this).css('transform').split(', ')[5]));
+        // console.log('css: ', clicked_matrix_part);
 
-
-        console.log('css: ', clicked_matrix_part);
-
-        if(clicked_matrix_part >= 0){
-            console.log('rotate ccw');
-        } else {
-            console.log('rotate cw');
-        }
+        // var dir_coefficient;
+        // if(clicked_matrix_part >= 0){
+        //     console.log('rotate ccw');
+        //     dir_coefficient = -1;
+        // } else {
+        //     console.log('rotate cw');
+        //     dir_coefficient = 1;
+        // }
 
        
         $('.tech').removeClass('selected');
 
-        var elt_count = $('.circle-container li.tech').length;
-  
+        for(var i = 0; i < elt_count; i++){
 
-        for(var i = 0; i <= elt_count; i++){
-            if(clicked_matrix_part >= 0){
-                console.log('rotate ccw');
-            } else {
-                console.log('rotate cw');
-            }
-            var end_rot = 360 / elt_count * (i - $elt_index) + 360;
-            // var end_rot = 360 / elt_count * (i - $elt_index);
+            // var end_rot = 360 / elt_count * (i - $elt_index);            //working
+            // var end_rot = 360 / elt_count * (i - $elt_index) + new_rot;
+            // var end_rot = 360 / elt_count * i + new_rot + last_rot;
+            rot_arr[i] += new_rot;
+
             
-            // end_rot = end_rot < 0 ? (end_rot + 360) % 360 : end_rot;
-            console.log('i: ', i, 'rot: ', end_rot);
-            $(`.circle-container > *:nth-of-type(${i+1})`).css({
-                'transform': `rotate(${end_rot}deg) translate(12.5em) rotate(-${end_rot}deg)`,
+            //important
+            // console.log('i: ', i, 'rot: ', end_rot);
+            
+            var $elt = $(`.circle-container > .tech:nth-of-type(${i+1})`);
+            var $attr = $elt.attr('loc');
+            // var new_loc = ( $elt.attr('loc') - $elt_index + elt_count ) % elt_count;
+            var new_loc = ( $elt.attr('loc') - loc_index + elt_count ) % elt_count;
+
+
+            $elt.attr('loc', new_loc).css({
+                // 'transform': `rotateZ(${end_rot}deg) translate(12.5em) rotateZ(${-1*end_rot}deg)`,
+                'transform': `rotateZ(${rot_arr[i]}deg) translate(12.5em) rotateZ(${-1*rot_arr[i]}deg)`,
                 // 'transition-duration': '0.5s'
             });
         }
+        console.log('rot_arr', rot_arr);
         
         $(this).toggleClass('selected');
-
-
-        // console.log('css: ', $(this).attr('transform'));
-        // console.log('parent: ', $(this).parent());
-
-        // var apparent_rot;
-        // curr_rot =  curr_rot || 0;
-        // apparent_rot = curr_rot % 360;
-        // if(apparent_rot < 0){
-        //     apparent_rot += 360;
-        // }
-        // if(apparent_rot < 180){
-
-        // }
-
-        // console.log('rot: ', rot);
-        // var $curr_selected = $('.selected').parent().index();
-        // console.log('curr_selected: ', $curr_selected);
-        
-        
-        // apparent_rot = 360 / elt_count * ($curr_selected - $elt_index) + 360;
-        // console.log('app_rot: ', apparent_rot);
-
-
-
-
     });
 }
