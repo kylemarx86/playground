9d8718c4985d99aa7d18d2ea9b4295b9e9518e94
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-08-08 13:03:59 -0700

    basic open and closing of envelope.

---
+++ envelope/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+//    console.log('ready');
+   $('.send').click(function(){
+    //    console.log('sent');
+       send_mail();
+   })
+});
+
+
+function send_mail(){
+    // $('.top').addClass('closed');
+    $('.top').toggleClass('closed');
+    $('.inside, .outside').toggleClass('invisible');
+    
+    // $('.top .inside, .top .outside').toggleClass('invisible');
+    
+}
