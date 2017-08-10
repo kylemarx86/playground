f989d0594c79e925b4e51f639d59d15a149ac246
kylemarx86 <kylemarx86@csu.fullerton.edu>
2017-08-09 17:45:56 -0700

    folding paper - top and bottom pieces fold in correct directions.

--- paper_folding/script.js
+++ paper_folding/script.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $('.fold-it').click(function(){
     console.log('fold it');
     // $('.piece:nth-of-type(1), .piece:nth-of-type(3)').toggleClass('folded');
-    // $('.top_container, .top').toggleClass('folded');
+    $('.top_container, .top').toggleClass('folded');
+    // $('.bottom_container, .bottom').toggleClass('folded');
     $('.bottom_container, .bottom').toggleClass('folded');
   });
 });
